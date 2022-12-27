50368ab29560257bb5192834437469c33a34ccdc
lilianiazzai <120421193+lilianiazzai@users.noreply.github.com>
2022-12-27 14:29:57 -0400

    Add files via upload

---
+++ atv_22122022/ex13.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int num;
+    int div = 0, res = 0;
+
+    printf("Digite um numero: ");
+    scanf("%i", &num);
+
+    while (div < num - 1) {
+        div++;
+        if (num % div == 0) {
+            res += div;
+        }
+    }
+
+    if (num == res) {
+        printf("Esse e um numero perfeito");
+    } else {
+        printf("Esse nao 3 um numero perfeito");
+    }
+    
+    return 0;
+}
